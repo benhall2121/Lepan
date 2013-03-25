10685b9a105dd9234f1443123be7f0cb5b95fd09
Ben Hall <benhall2121@yahoo.com>
2013-03-25 09:15:19 -0700

    benhall2121@yahoo.com initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lepan::Application.config.secret_token = '374094b8cd5cc7f26cf0df5533af08a7b694fc0c367a17a72b56b8766e116b6c4cf4580ac80d4e00f80f19f13b0b3efdde33b9df9d12f45a05ba2d4708d48800'
