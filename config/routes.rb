c29bcfa1bcc9d2e5c6d4115aa0506720dfe2240c
Brandon Gay <brandoncgay@gmail.com>
2013-03-29 17:50:43 -0700

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Lepan::Application.routes.draw do
+
   resources :quotes, only: :index
 
 
   resources :books, only: :index
 
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
 
   resources :users do
     resources :books do
