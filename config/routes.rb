49bb751247282c8ff33f76023b287aa49eb529f0
Brandon Gay <brandoncgay@gmail.com>
2013-03-29 19:27:49 -0700

    added sign up, log in, log out links to application layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Lepan::Application.routes.draw do
 
   resources :books, only: :index
 
+  resources :sessions
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
